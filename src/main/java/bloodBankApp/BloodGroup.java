3f98404bd5d62df42cb74434f6beeac37415ed04
Arjunkumar E <arjunae798@gmail.com>
2020-02-11 12:23:12 +0530

    Updated

---
+++ src/main/java/bloodBankApp/BloodGroup.java
@@ -0,0 +1,35 @@
+package bloodBankApp;
+
+public class BloodGroup {
+
+	private int empId;
+	private String empName;
+	private String email;
+	private String bloodGroup;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBloodGroup() {
+		return bloodGroup;
+	}
+	public void setBloodGroup(String bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
+	
+	
+}
