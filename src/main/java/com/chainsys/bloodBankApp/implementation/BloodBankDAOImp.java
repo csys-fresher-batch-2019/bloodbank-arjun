248f7413c5afe4214c99f747da92b26575528436
Arjunkumar E <arjunae798@gmail.com>
2020-02-11 14:02:04 +0530

    Add Model

--- src/main/java/com/chainsys/bloodBankApp/implementation/BloodBankDAOImp.java
+++ src/main/java/com/chainsys/bloodBankApp/implementation/BloodBankDAOImp.java
@@ -1,13 +1,12 @@
 package com.chainsys.bloodBankApp.implementation;
 
-import java.util.List;
-
+import java.util.ArrayList;
 import com.chainsys.bloodBankApp.dao.BloodGroupDAO;
 import com.chainsys.bloodBankApp.model.BloodGroup;
 
 public class BloodBankDAOImp implements BloodGroupDAO {
 
-	public List<BloodGroup> selectEmpBlood(String bloodGroup) throws Exception {
+	public ArrayList<BloodGroup> selectEmpBlood(String bloodGroup) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
