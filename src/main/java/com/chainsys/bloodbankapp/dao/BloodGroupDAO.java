347fe6de8a9da10cc4b6f98049919d155573e8a4
Arjunkumar E <arjunae798@gmail.com>
2020-02-17 10:22:02 +0530

    new jdbi

--- src/main/java/com/chainsys/bloodbankapp/dao/BloodGroupDAO.java
+++ src/main/java/com/chainsys/bloodbankapp/dao/BloodGroupDAO.java
@@ -2,6 +2,9 @@ package com.chainsys.bloodbankapp.dao;
 
 import java.util.List;
 
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
 import com.chainsys.bloodbankapp.exception.DbException;
 import com.chainsys.bloodbankapp.model.BloodGroup;
 
@@ -21,17 +24,23 @@ public interface BloodGroupDAO {
 	 */
 	List<BloodGroup> findByBloodGroup(String bloodGroup) throws DbException;
 	
+	@SqlQuery("select emp_id,emp_name,email,blood_group,active from blood_bank_app where blood_group=? and active =1")
 	int addEmployeeDetails(BloodGroup a) throws DbException;
 	
+	@SqlUpdate("update blood_bank_app set active=? where email=? and active != ?")
 	boolean updateDonateStatus(BloodGroup a) throws DbException;
 	
+	@SqlQuery("select emp_id,emp_name,email,blood_group,active from blood_bank_app where email=?")
 	public BloodGroup findByEmail(String email) throws DbException;
-	
+
+	@SqlUpdate("update blood_bank_app set email=? where emp_id=?")
 	public boolean updateEmail(BloodGroup a) throws DbException;
 	
 	public boolean updateBloodGroup(BloodGroup a) throws DbException;
 	
+	@SqlUpdate("delete from blood_bank_app where emp_id=?")
 	public boolean deleteEmployeeRecord(BloodGroup a) throws DbException;
 	
+	@SqlQuery("select emp_id,emp_name,email from blood_bank_app where blood_group is null")
 	public List<BloodGroup> findNullBloodRecords() throws DbException;
 }
